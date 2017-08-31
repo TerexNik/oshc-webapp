5fb0628260366a3c318d10c27962aac28c7cdbdc
mbikov <mbikov@tsconsulting.ru>
2017-08-31 19:23:06 +0300

    Restful with certificates. Doesnt' work

--- src/main/java/ru/OSHC/controller/CertificateController.java
+++ src/main/java/ru/OSHC/controller/CertificateController.java
@@ -1,11 +1,9 @@
 package ru.OSHC.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.OSHC.entity.Certificate;
 import ru.OSHC.service.CertificateService;
 
@@ -19,12 +17,62 @@ public class CertificateController {
     private CertificateService certificateService;
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
-    public @ResponseBody List<Certificate> getAllWorkersByDepId() {
+    public @ResponseBody List<Certificate> getAllCertificates() {
         try {
             return certificateService.getAll();
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+    public Certificate getCertificate(@PathVariable long id) {
+        try {
+            return certificateService.getByID(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void addCertificateView(@RequestBody Certificate certificate) {
+        try {
+            certificateService.add(certificate);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeCertificate(@RequestBody Certificate certificate) {
+        try {
+            certificateService.remove(certificate);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequestMapping(value = "/remove/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCertificateByID(@PathVariable long id) {
+        try {
+            certificateService.remove(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCertificate(@RequestBody Certificate certificate) {
+        try {
+            certificateService.update(certificate);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
