73a759fb4ffe2627636652ccd2453e8bed704722
Nikita Terekhin <nterekhin@tsconsulting.ru>
2017-09-04 14:32:04 +0300

    Some improvment

--- src/main/java/ru/OSHC/controller/CertificateController.java
+++ src/main/java/ru/OSHC/controller/CertificateController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.OSHC.entity.Certificate;
 import ru.OSHC.service.CertificateService;
+import ru.OSHC.service.DepartmentService;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -14,34 +15,26 @@ import java.util.List;
 @RestController
 @RequestMapping("/certificates")
 public class CertificateController extends BaseCRUDController<Certificate> {
-
     private static final Logger log = Logger.getLogger(CertificateController.class);
+
+    @Autowired
+    public CertificateController(CertificateService certificateService) {
+        setService(certificateService);
+    }
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     List<Certificate> getWithNames() {
-        try {
-            return getService().getWithNames("getCertificatesWithNames");
-        } catch (SQLException e) {
-            return null;
-        }
+        return getList("getCertificatesWithNames");
     }
 
     @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
     Certificate getById(@PathVariable Long id) {
-        try {
-            return getService().getById(id, "getCertificateById");
-        } catch (SQLException e) {
-            return null;
-        }
+        return getById(id, "getCertificateById");
     }
 
     @RequestMapping(value = "/remove/{id}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void deleteById(@PathVariable Long id) {
-        try {
-            getService().removeById(id, "getCertificateById");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+            deleteById(id, "getCertificateById");
     }
 }
