48b2130d3d72a16c077f6c30a3becae109ac7ce1
Pavel <dym91@list.ru>
2017-09-14 19:23:02 +0300

    POST & GRADE err

--- src/main/java/ru/OSHC/controller/PostController.java
+++ src/main/java/ru/OSHC/controller/PostController.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.OSHC.entity.Post;
+import ru.OSHC.exception.FileNotFoundException;
 import ru.OSHC.service.PostService;
 
+import javax.persistence.NoResultException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -27,72 +29,66 @@ public class PostController {
 
     /**
      * Добавление нового поста.
+     *
      * @param post - пост
      */
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    void addPost(@RequestBody Post post) {
-        try {
-            postService.add(post);
-        } catch (SQLException e) {
-            log.error("addPost", e);
-        }
+    void addPost(@RequestBody Post post) throws SQLException {
+        postService.add(post);
     }
 
     /**
      * Изменение данных о посте
+     *
      * @param post - пост
      */
     @RequestMapping(method = RequestMethod.PUT)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    void update(@RequestBody Post post) {
-        try {
-            postService.update(post);
-        } catch (SQLException e) {
-            log.error("updatePost", e);
-        }
+    void update(@RequestBody Post post) throws SQLException {
+        postService.update(post);
     }
 
     /**
      * Получение списка всех постов
+     *
      * @return возвращает список всех постов
      */
     @RequestMapping(method = RequestMethod.GET)
-    List getAll() {
-        try {
-            return postService.getAll("getPostsList");
-        } catch (SQLException e) {
-            log.error("getPostsList", e);
-            return null;
-        }
+    List getAll() throws SQLException {
+        return postService.getAll("getPostsList");
     }
 
     /**
      * Получение информации о посте с идентификатором {@link Post#id}
+     *
      * @param id - идентификатор поста
      * @return возвращает выбранный пост
      */
     @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
-    Post getPostById(@PathVariable Long id) {
+    Post getPostById(@PathVariable Long id) throws SQLException {
         try {
             return postService.getById(id, "getPostById");
-        } catch (SQLException e) {
-            log.error("getPostById", e);
-            return null;
+        } catch (NoResultException e) {
+            log.error("NoResultException in getPostById", e);
+            throw new FileNotFoundException("Такого поста не существует");
         }
     }
 
     /**
      * Удаление поста с выбранным идентификатором {@link Post#id}
+     *
      * @param id - идентификатор поста
      */
     @RequestMapping(value = "/remove/{id}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    void deleteById(@PathVariable Long id) {
+    void deleteById(@PathVariable Long id) throws SQLException {
         try {
             postService.removeById(id, "getPostById");
-        } catch (SQLException e) {
-            log.error("removePostById", e);
+        } catch (NoResultException e) {
+            log.error("NoResultException in removePostById", e);
+            throw new FileNotFoundException("Такого поста не существует");
         }
+
     }
 }
